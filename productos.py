488d0a7c11e27c3f37d7123e905f680971583994
Morales0 <moraless06@hotmail.com>
2026-04-27 16:22:51 -0600

    feat: :sparkles: Se agrega la funcion editar producto

--- productos.py
+++ productos.py
@@ -29,3 +29,30 @@ def agregar_producto():
         json.dump(datos, archivo, indent=4)
 
     print("Cafe registrado correctamente ")
+
+def editar_producto():
+    nombre_buscar = input("Ingrese el nombre del cafe a editar: ")
+
+    try:
+        with open("data/productos.json", "r") as archivo:
+            datos = json.load(archivo)
+    except:
+        print("No hay datos.")
+        return
+
+    for cafe in datos:
+        if cafe["nombre"] == nombre_buscar:
+            cafe["descripcion"] = input("Nueva descripcion: ")
+            cafe["precio"] = float(input("Nuevo precio: "))
+            cafe["tostado"] = input("Nuevo tostado: ")
+            cafe["cantidad"] = int(input("Nueva cantidad: "))
+
+            with open("data/productos.json", "w") as archivo:
+                json.dump(datos, archivo, indent=4)
+
+            print("Producto actualizado.")
+            return
+
+    print("Producto no encontrado.")
+
+editar_producto()
