d01bcae04db202b551057589059c223630c706f2
AlanGomez-Programmer <alan.s.g.j.2006@gmail.com>
2026-04-27 16:27:24 -0600

    feat: :sparkles: Se agrega la funcion listar productos

--- productos.py
+++ productos.py
@@ -29,3 +29,39 @@ def agregar_producto():
         json.dump(datos, archivo, indent=4)
 
     print("Cafe registrado correctamente ")
+
+def listar_productos():
+
+
+
+    try:
+        with open("data/productos.json", "r") as archivo:
+            datos = json.load(archivo) 
+    except:
+        datos = []
+
+    if len(datos) == 0:
+        print("ERROR: No hay productos")
+        regresar = input("Presione ENTER regresar...")
+        if regresar != "":
+            print("ERROR: Presione ENTER")
+        else:
+            return
+
+    contador = 1
+
+    for producto in datos:
+        nombre = producto["nombre"]
+        descripcion = producto["descripcion"]
+        precio = producto["precio"]
+        tostado = producto["tostado"]
+        cantidad = producto["cantidad"]
+
+        print("-"*30)
+        print(f"-> PRODUCTO {contador}:")
+        print(f"NOMBRE: {nombre}")
+        print(f"DESCRIPCIÓN: {descripcion}")
+        print(f"PRECIO: {precio}")
+        print(f"NIVEL DE TOSTADO: {tostado}")
+        print(f"CANTIDAD: {cantidad}")
+        contador += 1
